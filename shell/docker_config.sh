4e44e0eb4f84a0fda96ecd06ec220097914a5495
BEAR O8 <85358121+bear-o8@users.noreply.github.com>
2024-09-28 11:53:45 +0800

    Update docker_config.sh

--- shell/docker_config.sh
+++ shell/docker_config.sh
@@ -3,34 +3,23 @@ sudo apt-get update
 # 更新apt包索引
 sudo apt-get update
 
-# 为支持https
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-# 添加Docker GPG秘钥
-# 国内源
-curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-# 推荐国内源
-sudo add-apt-repository \
-	    "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
-	        $(lsb_release -cs) \
-		    stable"
-# 更新apt包索引
 sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+# step 2: 安装GPG证书
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+# Step 3: 写入软件源信息
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# Step 4: 更新并安装 Docker-CE
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 
-# 安装docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-# 添加镜像源
+# # 添加镜像源
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
-  "registry-mirrors": ["https://mirror.ccs.tencentyun.com"]
+  "registry-mirrors": ["https://em2r99v3.mirror.aliyuncs.com"]
 }
-EOF
+# EOF
 sudo systemctl enable docker
 sudo systemctl start docker
 
